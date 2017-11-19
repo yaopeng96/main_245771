43bc39a89b6220c25606a05d62f122dea2521875
OOI YAO PENG <33802685+yaopeng96@users.noreply.github.com>
2017-11-19 19:17:45 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Name: OOI YAO PENG");
+        System.out.println("Matric No: 245771");
+    }
+    //Main Method end
+} //Hello class end
